2e456c6d3f4b0b050cc02a6b6896ec79ce8289f0
Song Li <da_tou_li@yahoo.com>
2019-03-06 17:32:18 +0800

    Test on the two-way binding

--- client/src/example1/index.js
+++ client/src/example1/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import reducers from './reducers'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from './reducers';
+import { addNumberEvent } from './actions';
 
 import App from './components/App';
 import './styles/app-style.css';
 
 const store = createStore(reducers);
+
+store.dispatch(addNumberEvent(1223));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>,
 		document.getElementById('root'));
--- client/src/example2/components/App.js
+++ client/src/example2/components/App.js
@@ -1,7 +1,63 @@
 import React from 'react';
 
-const App = () => (
-  <div className="container">This is the example 2</div>
-);
+const width = {
+  width: '500px'
+}
+
+class App extends React.Component {
+	constructor(props) {
+    super(props);
+    
+		this.state = {
+      value: 'This is bullshit'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.clicked = this.clicked.bind(this);
+    this.keyup = this.keyup.bind(this);
+	}
+
+  handleChange(e) {
+    let v = e.target.value;
+    this.setState({value: v});
+  }
+
+  keyup(e) {
+    if(e.keyCode === 13) {
+      let v = this.state.value.trim();
+
+      this.setState({value: v});
+      alert('');
+      console.log(v);
+    }
+  }
+
+  clicked(e) {
+    let v = this.state.value;
+
+    console.log(v);
+  }
+
+	componentWillUnmount() {}
+
+	render() {
+    let t = this;
+    let s = t.state;
+
+		return <div className="container">
+            <div>This is the example 2</div>
+            <div>
+              <input type="text"
+                style={width}
+                value={s.value}
+                onChange={t.handleChange}
+                onKeyUp={t.keyup}></input>
+            </div>
+            <div>
+              <button type="button" style={width} onClick={t.clicked}>Check value</button>
+            </div>
+          </div>;
+	}
+}
 
 export default App;
