c2fee6337a664b4c83cd83fe013aeb115b9879d8
Song Li <da_tou_li@yahoo.com>
2019-03-28 11:01:40 +0800

    Added navigation sub-pages

--- client/src/example3/components/App.js
+++ client/src/example3/components/App.js
@@ -1,61 +1,20 @@
 import React from 'react';
-
-const width = {
-  width: '500px',
-  color: 'red'
-}
+import Page1 from './Page1';
 
 class App extends React.Component {
 	constructor(props) {
     super(props);
     
-		this.state = {
-      value: 'This is the stupid example 3'
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.clicked = this.clicked.bind(this);
-    this.keyup = this.keyup.bind(this);
+		this.state = {};
 	}
-
-  handleChange(e) {
-    let v = e.target.value;
-    this.setState({value: v});
-  }
-
-  keyup(e) {
-    if(e.keyCode === 13) {
-      let v = this.state.value.trim();
-
-      this.setState({value: v});
-      console.log(v);
-    }
-  }
-
-  clicked(e) {
-    let v = this.state.value;
-
-    console.log(v);
-  }
 
 	componentWillUnmount() {}
 
 	render() {
-    let t = this;
-    let s = t.state;
 
 		return <div className="container">
-            <div>This is the example 2</div>
-            <div>
-              <input type="text"
-                style={width}
-                value={s.value}
-                onChange={t.handleChange}
-                onKeyUp={t.keyup}></input>
-            </div>
-            <div>
-              <button type="button" style={width} onClick={t.clicked}>Check value</button>
-            </div>
+            <div>This is the navigation example</div>
+            <Page1></Page1>
           </div>;
 	}
 }
---
+++ client/src/example3/components/Page1.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const box = {
+    width: '500px',
+    height: '500px',
+    padding: '10px',
+    backgroundColor: 'red'
+};
+
+const Page1 = () => (
+    <div style={box}>
+        <div>This is the page No.1</div>
+    </div>
+);
+
+export default Page1;
