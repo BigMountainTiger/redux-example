7355a2347e9aa142c5325afca37577078361094b
Song Li <da_tou_li@yahoo.com>
2019-04-02 17:55:44 +0800

    Playing with the history

--- client/src/example3/components/App.js
+++ client/src/example3/components/App.js
@@ -4,9 +4,9 @@ import Page2 from './Page2';
 import Page3 from './Page3';
 
 const Page = {
-  Page1: 'Page1',
-  Page2: 'Page2',
-  Page3: 'Page3'
+  Page1: Page1,
+  Page2: Page2,
+  Page3: Page3
 }
 
 class App extends React.Component {
@@ -22,6 +22,8 @@ class App extends React.Component {
 
   navigate(page) {
     this.setState({page: page});
+
+    history.pushState({}, "", "#Astupid");
   }
 
 	componentWillUnmount() {}
@@ -33,15 +35,9 @@ class App extends React.Component {
               <button onClick={() => this.navigate(Page.Page1)}>Page 1</button>
               <button onClick={() => this.navigate(Page.Page2)}>Page 2</button>
               <button onClick={() => this.navigate(Page.Page3)}>Page 3</button>
+              <button onClick={() => alert(history.length)}>History length</button>
             </span>
-            {(() => {
-              switch (this.state.page) {
-                case Page.Page1:  return <Page1></Page1>;
-                case Page.Page2:  return <Page2></Page2>;
-                case Page.Page3:  return <Page3></Page3>;
-                default:  return <Page1></Page1>;
-              }
-            })()}
+            { React.createElement(this.state.page, null, null) }
           </div>;
 	}
 }
